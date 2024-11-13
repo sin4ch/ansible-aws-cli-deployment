9a42b4d2830f3ae42a978683ccec6c93b2541085
Osinachi Okpara <osinachi.okpara@eng.uniben.edu>
2024-11-13 22:58:54 +0100

    made necessary changes to the logic to gather key-pair names from user in script

--- ec2-instance-create.sh
+++ ec2-instance-create.sh
@@ -5,10 +5,15 @@ sudo apt-get update
 aws --version
 
 # Specify key pair location
-KEYPAIR="/home/osinachi/ansible-aws-cli-project/ssh-key/ansible-cli-login-project.pem"
+# KEYPAIR="/home/osinachi/ansible-aws-cli-project/ssh-key/ansible-cli-login-project.pem"
+
+read -p "What is your key-pair name?" KEYPAIR
 
 # Create 4 EC2 instances with ubuntu and security group specified/
 aws ec2 run-instances --image-id ami-0866a3c8686eaeeba --count 4  --instance-type t2.micro --key-name $KEYPAIR --region us-east-1 --security-group-ids sg-0376d79ee47f54755
 sudo apt-get update
+
+# Ansible playbook calls EC2 instances
+
 
 
