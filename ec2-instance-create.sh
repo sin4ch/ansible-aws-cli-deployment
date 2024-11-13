493f6c527169bef895c0b1b6a94a4c12ff3b8662
Osinachi Okpara <osinachi.okpara@eng.uniben.edu>
2024-11-13 22:18:33 +0100

    added aws command to create 4 EC2 Ubuntu t2.micro instances. Image ID specified, SG specified, keypair specified.

--- ec2-instance-create.sh
+++ ec2-instance-create.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 
+# Update and check version
+sudo apt-get update
 aws --version
-aws ec2 run-instances --image-id ami-
+
+# Specify key pair location
+KEYPAIR="/home/osinachi/ansible-aws-cli-project/ssh-key/ansible-cli-login-project.pem"
+
+# Create 4 EC2 instances with ubuntu and security group specified/
+aws ec2 run-instances --image-id ami-0866a3c8686eaeeba --count 4  --instance-type t2.micro --key-name $KEYPAIR --region us-east-1 --security-group-ids sg-0376d79ee47f54755
+sudo apt-get update
+
+
